86e61bccc2f6ba5c9484c5322dfcacbc758161be
Chandana <chandanar2706@gmail.com>
2021-09-15 15:11:49 +0530

    Third commit

--- GitHubReposJava/src/com/lti/demo/UserApp.java
+++ GitHubReposJava/src/com/lti/demo/UserApp.java
@@ -6,7 +6,7 @@ public class UserApp {
 		// TODO Auto-generated method stub
 		Product p=new Product(101, "Mobile", 5000);
 		System.out.println(p);
-		System.out.println("c");
+		System.out.println("R");
 	}
 
 }
