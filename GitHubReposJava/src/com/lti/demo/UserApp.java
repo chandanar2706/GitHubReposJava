4f286e542e515593f782800341f35a30b570f8b7
Chandana <chandanar2706@gmail.com>
2021-09-15 15:04:05 +0530

    First Commit "15/9/2021"

---
+++ GitHubReposJava/src/com/lti/demo/UserApp.java
@@ -0,0 +1,11 @@
+package com.lti.demo;
+
+public class UserApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Product p=new Product(101, "Mobile", 5000);
+		System.out.println(p);
+	}
+
+}
